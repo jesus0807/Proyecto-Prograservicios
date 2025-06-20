3b776b31f914b28b09cbce0fa3dcafd2637094fa
MiyagiCachorro24 <caballo24oct@gmail.com>
2025-06-19 19:41:39 -0600

    parte 2 del script

--- servicios.sh
+++ servicios.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 servicios=("ssh" "cron" "nginx") #lista de servicios que se trabajaran
+
+for servicio in "${servicios[@]}"; do #se recorrera los servicios en la lista
+	systemctl is-active --quiet $servicio #se verificara si el servicio esta activo
+	if [ $? -ne 0 ]; then
+		echo "Heyyy tu el que estas viendo esto, !el servicio $servicio esta detenido o caido"
+	else
+		echo "todo ta bien =)"
+	fi
+done
