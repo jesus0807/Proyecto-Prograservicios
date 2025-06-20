f46969c0051b6973485ea87394841915c83f8b30
MiyagiCachorro24 <caballo24oct@gmail.com>
2025-06-20 15:19:34 -0600

    Documentación y cambios del script

--- servicios.sh
+++ servicios.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-# Los datos del bot de telegram
-source "$(dirname "$0")/config.txt"
+# Carga las variables del archivo config.txt
+source "$(dirname "$0")/config.txt" # dirname "$0" : obtienen la ruta donde este hubicado el archivo
 
-mensaje_telegram() { #funcionamiento para enviar las notificaciones
-	local mensaje="$1"
+mensaje_telegram() { #funcion para enviar las notificaciones
+	local mensaje="$1" # recibe como un parametro 
 
+# chat_id: del grupo donde se encuentra el bot y el mensaje que se ara llegar a telegram
 	curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
 		-d chat_id="$CHAT_ID" \
 		-d text="$mensaje"
 }
 
 # Butle principal
-for servicio in "${SERVICIOS[@]}"; do #se recorrera los servicios en la lista
-	systemctl is-active --quiet $servicio #se verificara si el servicio esta activo
+for servicio in "${SERVICIOS[@]}"; do #se recorrera los servicios en la lista, que definimos en la seccion de SERVICIOS en config.txt
+	systemctl is-active --quiet $servicio # se verificara si el servicio esta activo
 
-	if [ $? -ne 0 ]; then # si esta inactivo (status diferente de 0) el servicio esta detendio
-		systemctl restart "$servicio" #intenta reiniciar el servicio
-		sleep 1 # espera breve para el reinicio para el efecto
+	if [ $? -ne 0 ]; then # si esta inactivo (codigo diferente de 0) se intentara reiniciar
+		systemctl restart "$servicio" #  reinicia el servicio, si c cumple la condicion
+		sleep 1 # espera 1 segundo para darle tiempo al servicio
 
-		systemctl is-active --quiet "$servicio" # verifica nuevamente si el servicio se activo
+		systemctl is-active --quiet "$servicio" # vuelve a verificar si el servicio esta activo ( se reinicio automaticamente)
 
-		if [ $? -eq 0 ]; then # salta un mensaje se c reinico
-		mensaje=" onni-chan el servicio $servicio se detubo o cayo, pero se reiniciooo"
+		if [ $? -eq 0 ]; then # condicion donde el servicio se reinicio automaticamente ( codigo mayor a 0 ) se reinicio exitosamente
+		mensaje=" onni-chan el servicio $servicio se detubo o cayo, pero se reiniciooo" # notifica que c reinicio automaticamente
+		echo "$mensaje" 
+		else # sino logro reiniciar el servicio
+		mensaje "sempai el servicio  $servicio no c pudo reiniar" # manda un mensaje donde notifica que no se pudo reinicar
 		echo "$mensaje"
-		else
-		mensaje "sempai el servicio  $servicio no c pudo reiniar" # salta el mensaje si no c pudo reinicia
-		echo "$mensaje"
-		fi
+		fi # final de la condicion de reinicio
 
-		mensaje_telegram "$mensaje" # envia el mensaje a telegram
-
-	else
-		echo " el servicio esta funcionando bien"
-	fi
-done
+		mensaje_telegram "$mensaje" # envia una notificacion si el servicio se (reinicio/o fallo)
+	fi 
+done #final del bucle
