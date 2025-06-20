32d46a3d90db2405eb1ef4d966c10c1d73140de8
MiyagiCachorro24 <caballo24oct@gmail.com>
2025-06-19 19:47:59 -0600

    parte 3 del script: verificacion de los estados del servicio (activo/inactivo)

--- servicios.sh
+++ servicios.sh
@@ -4,8 +4,9 @@ servicios=("ssh" "cron" "nginx") #lista de servicios que se trabajaran
 
 for servicio in "${servicios[@]}"; do #se recorrera los servicios en la lista
 	systemctl is-active --quiet $servicio #se verificara si el servicio esta activo
-	if [ $? -ne 0 ]; then
-		echo "Heyyy tu el que estas viendo esto, !el servicio $servicio esta detenido o caido"
+
+	if [ $? -ne 0 ]; then # si esta inactivo (status diferente de 0)
+		echo "Heyyy tu el que estas viendo esto, !el servicio $servicio esta detenido o caido, se esta tratando de reiniciar"
 	else
 		echo "todo ta bien =)"
 	fi
