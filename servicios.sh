cd841a982b341bebcbb04efbc9592e6a8ada8acc
MiyagiCachorro24 <caballo24oct@gmail.com>
2025-06-19 20:18:05 -0600

    parte 6: ingresar la forma de envio de mensaje

--- servicios.sh
+++ servicios.sh
@@ -5,6 +5,13 @@ CHAT_ID="-1002891538692"
 
 servicios=("ssh" "cron" "nginx") #lista de servicios que se trabajaran
 
+mensaje_telegram() { #funcionamiento para enviar las notificaciones
+	local mensaje="$1"
+	curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage \
+		-d chat_id="$CHAT_ID" \
+		-d text="$mensaje"
+}
+
 for servicio in "${servicios[@]}"; do #se recorrera los servicios en la lista
 	systemctl is-active --quiet $servicio #se verificara si el servicio esta activo
 
@@ -16,11 +23,16 @@ for servicio in "${servicios[@]}"; do #se recorrera los servicios en la lista
 		systemctl is-active --quiet $servicio
 
 		if [ $? -eq 0 ]; then
-		echo " onni-chan el servicio $servicio se reiniciooo"
+		mensaje=" onni-chan el servicio $servicio se reinicio, pero se reiniciooo"
+		echo "$mensaje"
 		else
-		echo "sempai el servicio  $servicio no c pudo reiniar"
+		mensaje "sempai el servicio  $servicio no c pudo reiniar"
+		echo "$mensaje"
 		fi
+
+		mensaje_telegram "$mensaje"
+
 	else
 		echo " el servicio esta funcionando bien"
 	fi
-
+done
