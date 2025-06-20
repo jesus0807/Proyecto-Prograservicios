6738da1088ca38f771a0c693fa560484deba874d
MiyagiCachorro24 <caballo24oct@gmail.com>
2025-06-19 19:56:47 -0600

    parte 4 del script: reinicio del servicio automaticamente

--- servicios.sh
+++ servicios.sh
@@ -7,7 +7,17 @@ for servicio in "${servicios[@]}"; do #se recorrera los servicios en la lista
 
 	if [ $? -ne 0 ]; then # si esta inactivo (status diferente de 0)
 		echo "Heyyy tu el que estas viendo esto, !el servicio $servicio esta detenido o caido, se esta tratando de reiniciar"
+		systemctl restart $servicio
+		sleep 1
+
+		systemctl is-active --quiet $servicio
+
+		if [ $? -eq 0 ]; then
+		echo " onni-chan el servicio $servicio se reiniciooo"
+		else
+		echo "sempai el servicio  $servicio no c pudo reiniar"
+		fi
 	else
-		echo "todo ta bien =)"
+		echo " el servicio esta funcionando bien"
 	fi
-done
+
