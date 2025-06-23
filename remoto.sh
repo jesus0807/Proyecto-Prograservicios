9253fc80b0a2a1afafce2619834fb7af84125084
DonJDope <zS23021454@estudiantes.uv.mx>
2025-06-23 01:05:23 -0600

    Añadiendo los hots y probando remoto.sh

--- remoto.sh
+++ remoto.sh
@@ -23,16 +23,37 @@ if [ ! -f "$Archivo" ]; then
 	exit 1
 fi
 
-# Transferir el script al servidor remoto con scp
-echo " Copiandoel '$criptT' al servidor remoto..."
-scp "$Script" "$Usuario@$IP:/tmp/" || {
-  echo " Error durante la copia con scp."
-  exit 1
-}
-
 #Confirmacion de que funcionan lso dos archivos
 echo "Archivos verificados, haciendo la ejecucion remota de $Script"
 
-# Ejecutar el script remotamente con ssh
-echo " Ejecutando '$cript' en el servidor remoto..."
-ssh "$Usuario@$IP" "bash /tmp/$(basename "$Script")"
+#CReamos la carpeta donde se ubicaran los reportes de las ejecuciones remotas
+mkdir -p Reportes
+
+#Lee la IP del archivo ips.txt
+while IFS= read -r IP; do #IFS avriable que analiza cada linea interna del archivo ips.txt
+	echo "Procesando el host con ip: $IP"
+
+	#Verifica si la línea no está vacía si no, seguira buscando
+	if [ -z "$IP" ]; then
+    		echo "Buscando una IP"
+    		continue
+  	fi
+
+  	#Copia el script al host remoto
+  	scp "$Script" "$Usuario@$IP:/tmp/" 2>> reportes_remotos/$IP.log
+  	if [ $? -ne 0 ]; then
+    		echo "Falló la copia a $IP" | tee -a reportes_remotos/$IP.log
+    		continue
+  	fi
+
+  	#Ejecuta el script 
+  	echo "Ejecutando script en $IP..."
+  	ssh "$Usuario@$IP" "bash /tmp/$(basename "$Script")" >> reportes_remotos/$IP.log 2>&1
+
+  	echo "Finalizado $IP" | tee -a reportes_remotos/$IP.log
+	echo "---------------------------------------------" >> reportes_remotos/$IP.log
+
+	#Crea el log del host donde se ejcuta el script
+	Log="Reportes/$IP.log"
+	echo "🕒 Fecha: $(date '+%Y-%m-%d %H:%M')" > "$Log"
+done < "$Archivo" 
