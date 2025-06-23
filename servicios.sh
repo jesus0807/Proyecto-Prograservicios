f72c975dbd1c8085c97b13b0135cbfaee0369d2f
MiyagiCachorro24 <caballo24oct@gmail.com>
2025-06-22 20:58:52 -0600

    modificacion del script para mas claridad

--- servicios.sh
+++ servicios.sh
@@ -23,8 +23,7 @@ for servicio in "${SERVICIOS[@]}"; do #se recorrera los servicios en la lista, q
 		systemctl is-active --quiet "$servicio" # vuelve a verificar si el servicio esta activo ( se reinicio automaticamente)
 
 		if [ $? -eq 0 ]; then # condicion donde el servicio se reinicio automaticamente ( codigo mayor a 0 ) se reinicio exitosamente
-		mensaje=" onni-chan el servicio $servicio se detubo o cayo, pero se reiniciooo, el reinicio se realizo el dia $(date '+%Y-%m-%d') en la hora $(date '+%H:%M:%S')" # notifica que c reinicio automaticamente
-
+		mensaje=" onni-chan, el servicio $servicio se detubo o se cayo, pero se reinicio correctamente, el reinicio se realizo el dia $(date '+%Y-%m-%d') en la hora $(date '+%H:%M:%S')" # notifica que c reinicio automaticamente
 		echo "$mensaje"
 		else # sino logro reiniciar el servicio
 		mensaje "sempai el servicio  $servicio no c pudo reiniar" # manda un mensaje donde notifica que no se pudo reinicar
@@ -33,7 +32,6 @@ for servicio in "${SERVICIOS[@]}"; do #se recorrera los servicios en la lista, q
 
 		mensaje_telegram "$mensaje" # envia una notificacion si el servicio se (reinicio/o fallo)
 		echo "$mensaje"
-
 	else
 		echo " el servicio esta funcionando bien " #manda un mensaje a la terminal que los servicios estan bien (en el caso que ningun servicio este fallando o se detubo)
 	fi
