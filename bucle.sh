fda3b2a3252bbbf5e026f8b07212e23826265d54
MiyagiCachorro24 <caballo24oct@gmail.com>
2025-06-23 16:39:39 -0600

    se agregara el script bucle.sh
    sirve para el funcionamiento automático del script servicios.sh

---
+++ bucle.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do # inicio del bucle
+	sudo /home/irvin/GitHub/Proyecto-Prograservicios/servicios.sh # directorio de la hubicacion del script para el uso del bucle
+	sleep 10 # hace una breve espera de 10 seg, para poder ejecutar el script
+done # fin del bucle
